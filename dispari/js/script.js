ece2b6139329430cee2508e25a68c26bc362ae6b
SerenaMalusa <sere.malu@gmail.com>
2024-01-17 18:06:09 +0100

    feat(dispari): add function to get a random number

--- dispari/js/script.js
+++ dispari/js/script.js
@@ -7,5 +7,19 @@ const resultOputputEl = document.getElementById('result-output');
 
 // 2. al click del bottone
 sendBtn.addEventListener('click', function() {
-    console.log('click');
+    
+    // 3. invoco la funzione numero random
+    const pcNumber = getRandomNum(5,1);
+
+    
+
 })
+
+function getRandomNum (max, min = 0) {
+
+    // a. genera un numero random con parametri min e max
+    const randomNum = Math.floor(Math.random() * (max +1 - min) + min);
+    // b. restituisce il numero random
+    return randomNum;
+
+}
