69950710888d8a68828e2e9315ec35cde8446042
SerenaMalusa <sere.malu@gmail.com>
2024-01-17 18:01:15 +0100

    feat(disperi): add event listener on button click

--- dispari/js/script.js
+++ dispari/js/script.js
@@ -5,3 +5,7 @@ const numInputEl = document.getElementById('num-imput');
 const sendBtn = document.getElementById('send-button');
 const resultOputputEl = document.getElementById('result-output');
 
+// 2. al click del bottone
+sendBtn.addEventListener('click', function() {
+    console.log('click');
+})
