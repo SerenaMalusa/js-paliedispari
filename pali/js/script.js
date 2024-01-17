c95b8b9758838ae28de5e16fc6fabfa93095243e
SerenaMalusa <sere.malu@gmail.com>
2024-01-17 16:49:18 +0100

    feat(pali): add dom elements and button event on click

---
+++ pali/js/script.js
@@ -0,0 +1,9 @@
+// 1. prendo dal dom gli elementi che mi servono
+const wordInputEl = document.getElementById('word-input');
+const sendBtnEl = document.getElementById('send-button');
+const resultOutputEl = document.getElementById('result-output');
+
+// 2. al click del bottone
+sendBtnEl.addEventListener('click', function () {
+    console.log('click');
+})
